314392492c570e709b4b45ac1d5db5058b8a06c4
theschles <phil@schlesingerfamily.net>
2019-12-16 13:38:41 -0800

    Update build-run-remarkable.sh

--- build-run-remarkable.sh
+++ build-run-remarkable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-dockcross-linux-armv7-rust ./build.sh
-dockcross-linux-armv7-rust ./dist.sh
+../dockcross/dockcross-linux-armv7-rust ./build.sh
+../dockcross/dockcross-linux-armv7-rust ./dist.sh
 
 DEVICE_IP="10.11.99.1"
 ssh root@$DEVICE_IP 'systemctl stop xochitl || true ; killall -9 plato || true; killall -9 draft || true; systemctl stop xochitl || true'
